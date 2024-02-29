29b4b3a774cc3acd166d1ac926341529c37fa437
Michaël Giovanni Jules <julesmichaelgiovanni@gmail.com>
2024-02-29 18:35:30 +0400

    smoll formatting

--- templates/task_templ.go
+++ templates/task_templ.go
@@ -71,7 +71,7 @@ func TaskListComponent(tasks []task.Task) templ.Component {
 			var templ_7745c5c3_Var3 string
 			templ_7745c5c3_Var3, templ_7745c5c3_Err = templ.JoinStringErrs(task.Content)
 			if templ_7745c5c3_Err != nil {
-				return templ.Error{Err: templ_7745c5c3_Err, FileName: `templates/task.templ`, Line: 17, Col: 24}
+				return templ.Error{Err: templ_7745c5c3_Err, FileName: `templates/task.templ`, Line: 20, Col: 24}
 			}
 			_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString(templ.EscapeString(templ_7745c5c3_Var3))
 			if templ_7745c5c3_Err != nil {
